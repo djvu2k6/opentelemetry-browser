bae006307fb0472afc3d9b5df3505023dfbc405a
Dhananjay Mohan <dhananjaymohan67@gmail.com>
2026-05-09 03:27:41 +0530

    Remove duplicate export of defaultSanitizeUrl

--- packages/instrumentation/src/navigation/index.ts
+++ packages/instrumentation/src/navigation/index.ts
@@ -2,10 +2,9 @@
  * Copyright The OpenTelemetry Authors
  * SPDX-License-Identifier: Apache-2.0
  */
-
+export { defaultSanitizeUrl } from '../utils/index.ts';
 export { NavigationInstrumentation } from './instrumentation.ts';
 export type {
   NavigationInstrumentationConfig,
   NavigationType,
 } from './types.ts';
-export { defaultSanitizeUrl } from '../utils/index.ts';

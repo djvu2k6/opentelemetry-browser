277751101a9baaa4b0d3cfd09450da20733a6c8f
Dhananjay Mohan <dhananjaymohan67@gmail.com>
2026-05-11 04:10:29 +0530

    Export defaultSanitizeUrl from utils in index.ts

--- packages/instrumentation/src/navigation/index.ts
+++ packages/instrumentation/src/navigation/index.ts
@@ -2,7 +2,6 @@
  * Copyright The OpenTelemetry Authors
  * SPDX-License-Identifier: Apache-2.0
  */
-export { defaultSanitizeUrl } from '../utils/index.ts';
 export { NavigationInstrumentation } from './instrumentation.ts';
 export type {
   NavigationInstrumentationConfig,
